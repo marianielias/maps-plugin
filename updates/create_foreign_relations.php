44ad4224ecbf43879d22441c7932e4401325990f
FlingeR <marianielias@gmail.com>
2016-02-26 10:44:50 -0300

    Foreign Relations

---
+++ updates/create_foreign_relations.php
@@ -0,0 +1,18 @@
+<?php namespace Fencus\Maps\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class CreateForeignRelations extends Migration
+{
+
+    public function up()
+    {
+        Schema::table('fencus_maps_map_location', function ($table)
+        {
+        	$table->foreign('map_id')->references('id')->on('fencus_maps_maps')->onDelete('cascade');
+        	$table->foreign('location_id')->references('id')->on('fencus_maps_locations')->onDelete('cascade');
+        });
+    }
+
+}
